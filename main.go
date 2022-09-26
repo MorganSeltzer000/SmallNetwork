57f480d7058b919d16be9ec8f33cf5048144bb39
Yi Li <59032869+licds@users.noreply.github.com>
2022-09-25 23:26:21 -0400

    Complete most comments, need more for main()

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ var procDict = make(map[string][2]string) //map PID to [destIP,port]
 
 func unicast_send(myPID string, destination string, message string) {
 	fmt.Println("Sending to", destination)
+	// Dial destination to estableish a connection
 	connection, err := net.Dial("tcp", destination)
 	defer connection.Close()
 	if err != nil {
@@ -24,11 +25,11 @@ func unicast_send(myPID string, destination string, message string) {
 		return
 	}
 	startTime := time.Now().UnixMilli()
-	//because we're simulating network delay, we print the message actually before we send
-	//and then just have an error message if the sending actually fails
+	// Because we're simulating network delay, we print the message before we send
+	// and then just have an error message if the sending actually fails
 	fmt.Printf("Sent \"%s\" to process %s, system time is %d\n", message, destination, startTime)
 	delay := int64(rand.Intn(defaultDelay[1]+defaultDelay[0]) - defaultDelay[0]) //so can compare w/ startTime
-	//doing it this way, since context switching could happen
+	// Doing it this way, since context switching could happen
 	for time.Now().UnixMilli()-startTime < delay {
 	}
 	n, err := fmt.Fprintf(connection, myPID+" "+message+"\n")
@@ -39,7 +40,7 @@ func unicast_send(myPID string, destination string, message string) {
 
 func unicast_receive(source, message string) {
 	// This is designed to confirm that the message is received properly by printing out the time
-	//The actual processing of information is taken care in listener, please see specification below.
+	// The actual processing of information is taken care in listener, please see specification below.
 	fmt.Printf("Received \"%s\" from process %s, system time is %d \n", message, source, time.Now().UnixMilli())
 }
 
@@ -51,29 +52,28 @@ func listener(port string) {
 		panic("Unable to listen on the port")
 	}
 	fmt.Println("Im listening!")
-	//fmt.Println(listener) //This line was used to avoid the "variable declared but not used mistake"
 	for {
-		//This accepts the next incoming call for the connection, and an error message will be printed if connection is not established correctly.
+		// Establish connection
 		connection, err := l.Accept()
 		fmt.Println("Accepted the connection")
 		if err != nil {
 			fmt.Printf("Unable to connect to listener: %s\n", l)
 			return
 		}
-		//Read the message that is used for connection purposes
+		// Read the message from the connection
 		netData, err := bufio.NewReader(connection).ReadString('\n')
 		if err != nil {
 			fmt.Printf("Unable to read from: %s\n", connection)
 			return
 		}
-		//In case there were any unhelpful leading and tailing emptyspace, we will trim all the way through to retain useful information
+		// In case there were any unhelpful leading and tailing emptyspace, we will 
+		// trim all the way through to retain useful information
 		rawMessage := strings.TrimSpace(string(netData))
-		//The netData has 2 parts: the source and the message, seperated by space, hence by splitting can obtain such information correctly
+		// The netData has 2 parts: the source and the message, seperated by space, 
+		// hence by splitting can obtain such information correctly
 		source := strings.Split(rawMessage, " ")[0]
 		message := strings.Split(rawMessage, " ")[1]
-
-		unicast_receive(source, message) // Note that this is to make sure it is received properly.
-
+		unicast_receive(source, message)
 		connection.Close()
 	}
 }
@@ -81,8 +81,10 @@ func listener(port string) {
 func main() {
 	/*
 		   There are a few error cases that needs to be handeled:
-		1: When the user does not prompt an argument(the connected port) after the executable, the process will terminate automatically
-		2: When the user does not input a correct form of argument after the executable(e.g: an argument that is DNE in the config.txt), program will also terminate
+		1: When the user does not prompt an argument(the connected port) after the 
+		   executable, the process will terminate automatically
+		2: When the user does not input a correct form of argument after the executable
+		   (e.g: an argument that is DNE in the config.txt), program will also terminate
 	*/
 	if len(os.Args) < 2 {
 		panic("Incorrect args: should be ./[PROGNAME] line_to_read")
@@ -91,12 +93,13 @@ func main() {
 	if err != nil {
 		panic("Incorrect args: should be ./[PROGNAME] line_to_read")
 	}
+	// Open the config file
 	file, err := os.Open("config.txt")
-
 	if err != nil {
 		panic("Unable to open config file")
 	}
 	defer file.Close()
+	// Read the config file
 	scanner := bufio.NewScanner(file)
 
 	scanner.Scan()
@@ -134,16 +137,19 @@ func main() {
 		panic("line_to_read is larger than the size of the config file")
 	}
 
+	// Start the listener and watch for messages
 	go listener(procDict[myPID][1])
 	var stdSlice []string
 	stdScan := bufio.NewScanner(os.Stdin)
 	for stdScan.Scan() {
 		stdSlice = strings.Split(stdScan.Text(), " ")
+		// If the user inputs "send", then the program will send the message to the ID
 		if len(stdSlice) < 3 || stdSlice[0] != "send" {
 			fmt.Println("Incorrect args. Format as 'send ID MESSAGE'")
 			continue
 		}
 		fmt.Println("Will be sending to", stdSlice[1])
+		// Send the message to the destination (IP followed by port)
 		go unicast_send(myPID, procDict[stdSlice[1]][0]+":"+procDict[stdSlice[1]][1], strings.Join(stdSlice[2:], ""))
 	}
 	fmt.Println("at the end, truly")
